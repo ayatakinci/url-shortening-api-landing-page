3fa666954006268c44e32a304835c2d10e7bbcab
Adryan Rosa <adryanrosa374@gmail.com>
2021-08-15 22:44:02 -0300

    Ajusta markup de Hero

--- src/sections/Hero/index.jsx
+++ src/sections/Hero/index.jsx
@@ -6,16 +6,16 @@ import image from '../../images/illustration-working.svg';
 class Hero extends Component {
 	render() {
 		return (
-			<div className="hero-container">
-				<section className="hero">
-					<div className="text-container">
+			<section className="hero">
+				<div className="hero__grid">
+					<div className="hero__text-container">
 						<h2>More than just shorter links</h2>
 						<p>Build your brand’s recognition and get detailed insights on how your links are performing.</p>
 						<Button text="Get Started" />
 					</div>
-					<div className="image-container"><img src={image} alt="Working" /></div>
-				</section>
-			</div>
+					<div className="hero__image-container"><img src={image} alt="Working" /></div>
+				</div>
+			</section>
 		);
 	}
 }
